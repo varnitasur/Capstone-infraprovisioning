19d26aa41b99edfa3a2c2571e1717f427cef9563
varnitasur <58681874+varnitasur@users.noreply.github.com>
2020-02-17 12:50:56 +0530

    Update terraform_file.tf

--- Terrform/terraform_file.tf
+++ Terrform/terraform_file.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-08cec7c429219e339"
+  ami                    = "ami-08cec7c429219e33"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
